e9ce5a0c1dc751817ee46ef7397fabef477b9efd
blackinitial <alhanif.blackinitial@gmail.com>
2023-04-07 09:12:22 +0700

    profile page with ssg helper trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,16 @@ import "~/styles/globals.css";
 import { ClerkProvider } from '@clerk/nextjs';
 import type { AppProps } from 'next/app'
 import { Toaster } from "react-hot-toast";
-
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
   return (
     <ClerkProvider {...pageProps} >
+      <Head>
+        <title>Kicau App</title>
+        <meta name="description" content="🕊" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center"/>
       <Component {...pageProps} />
     </ClerkProvider>
