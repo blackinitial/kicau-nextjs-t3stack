3f70b39da1723edd3b65d3f29dab6891478847b7
blackinitial <alhanif.blackinitial@gmail.com>
2023-04-09 17:08:20 +0700

    change flatMap

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -19,22 +19,27 @@ const addUserDataToPosts = async (posts: Post[]) => {
 
   // console.log(users);
 
-  return posts.map((post) => {
+  return posts.flatMap((post) => {
     const author = users.find((user) => user.id === post.authorId);
 
-    if (!author || !author.username)
-      throw new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: "Author for post not found",
-      });
+    if (!author || !author.username) {
+      console.error("AUTHOR NOT FOUND", post);
+      return [];
+      // throw new TRPCError({
+      //   code: "INTERNAL_SERVER_ERROR",
+      //   message: `Author for post not found. POST ID: ${post.id}, USER ID: ${post.authorId}`,
+      // });
+    }
 
-    return {
-      post, 
-      author: {
-        ...author,
-        username: author.username
+    return [
+      {
+        post, 
+        author: {
+          ...author,
+          username: author.username
+        }
       }
-    };
+    ];
   });
 }
 
